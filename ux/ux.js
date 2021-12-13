b2e65c768c1ae71decbf0b90c60de885edfb01ff
Yazeed Alzahrani <y2-alzahrani@fet-2q025-08.uwe.ac.uk>
2021-12-13 19:14:40 +0000

    h

---
+++ ux/ux.js
@@ -0,0 +1,19 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+ /*in text*/
+ var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+}
